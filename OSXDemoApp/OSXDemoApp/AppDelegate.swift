468ee9b9698417c5d8df4593f77e8f94973f9c68
andrebouchard <andre.b.mtl@gmail.com>
2017-12-07 02:23:18 -0500

    xcode project start

---
+++ OSXDemoApp/OSXDemoApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSXDemoApp
+//
+//  Created by reisling on 2017-12-07.
+//  Copyright © 2017 andre.bouchard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSXDemoApp/OSXDemoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSXDemoApp
+//
+//  Created by reisling on 2017-12-07.
+//  Copyright © 2017 andre.bouchard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
